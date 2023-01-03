7de05b361b5f4694f58b897512832483ea573750
Jon Aldekoa <jaldekoa@gmail.com>
2023-01-03 20:55:43 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -3,27 +3,24 @@ require("jpeg")
 require("grid")
 require("ggplot2")
 
-# Set the path of the script and the data
-setwd("C:/Users/jalde/Desktop/Bill")
 
 # Load and format the raw data
-df <- read.csv("data.csv", sep=";", dec=",") # Spanish Excel save csv data with ";" as separator and "," for decimals
-colnames(df) <- c("Date", "HIPC.Index")
-df$Date <- as.Date(df$Date, "%d/%m/%Y")
+URL <- "https://sdw-wsrest.ecb.europa.eu/service/data/ICP/M.ES.N.000000.4.INX?format=csvdata"
+df <- read.csv(URL)[c("TIME_PERIOD", "OBS_VALUE")]
 
-# Modify the data according to your needs
-# I will take the beginning of my data series from the founding date of the European Central Bank (June 1, 1998).
-# Then, I calculate the loss of purchasing power from the beginning. 
-df <- df[df$Date >= as.Date("1998-05-31"),]
-df$PPP <- df$HIPC.Index[1]/df$HIPC.Index
+df$TIME_PERIOD <- seq.Date(as.Date("1996-02-01"), by="month", length.out = nrow(df)) - 1
+df$OBS_VALUE <- as.numeric(df$OBS_VALUE)
+
+df <- df[df$TIME_PERIOD >= as.Date("1999-12-31"),]
+df$PPP <- df$OBS_VALUE[1]/df$OBS_VALUE
 
 
-#Load the banknote image
+# Load the banknote image
 bill <- readJPEG("Euro Banknote.jpg")
 bill <- rasterGrob(bill, width=unit(1,"npc"), height=unit(1,"npc"), interpolate=TRUE) 
 
 # Draw the chart
-graph <- ggplot(df, aes(x = Date, y = PPP)) +
+graph <- ggplot(df, aes(x = TIME_PERIOD, y = PPP)) +
   
   # First draw the banknote. You must set the starting position of axis Y "ymin"
   annotation_custom(bill, xmin = -Inf, xmax = Inf, ymin = 0, ymax = Inf) +
@@ -31,23 +28,35 @@ graph <- ggplot(df, aes(x = Date, y = PPP)) +
   # Then draw over it the white ribbon from the top of the graph to the data series
   geom_ribbon(aes(ymin = PPP, ymax = Inf), fill = "white") +
   geom_line(size=1, colour="#000000") +
+  geom_hline(yintercept=tail(df$PPP, 1), linetype="dashed") + 
   
   # Add the appropriate scale
   scale_x_date(date_breaks="1 year",date_labels = "%Y", expand=c(0,0)) +
-  scale_y_continuous(expand=c(0,0), labels=scales::percent, limits = c(0,1), breaks=seq(0, 1, 0.1)) +
+  scale_y_continuous(expand = c(0,0), labels = scales::percent, limits = c(0,1), breaks = seq(0, 1, 0.1)) +
   
   # Put the labels you want to the chart
   labs(
-    title = "Purchasing Power of Euro",
-    subtitle = "Founding date of the ECB (June 1, 1998) = 100%",
+    title = "Poder Adquisitivo del €uro en España",
+    subtitle = "Desde 2000 hasta 2022. 1 de enero de 2000 = 100%",
     x="",
     y="",
-    caption="Data: HICP - Overall Index of Euro Area. Source: ECB Statistical Data Warehouse.") +
-  
+    caption="Elaboración propia.Fuente: ECB Statistical Data Warehouse. Creado por @jaldeko.") +
+   
   # Add a theme and other aesthetics modifications
   theme_minimal() +
-  theme(plot.title = element_text(face="bold", hjust=0.5, size=rel(1.5)),
-        plot.subtitle = element_text(face="italic", hjust=0.5))
+  theme(plot.caption = element_text(face="italic"), 
+        plot.title = element_text(face="bold", hjust=0.5, size=rel(1.5)),
+        plot.subtitle = element_text(face="italic", hjust=0.5),
+        axis.text.x = element_text(colour = "black", hjust=-0.5, face="bold"),
+        axis.text.y = element_text(colour = "black", face="bold"),
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour="black"),
+        axis.ticks = element_line(colour="black"),
+        legend.key.height = unit(0.75, 'cm'),
+        legend.text = element_text(colour = "black", face="italic", size=rel(0.75)))
+
 
 # Plot it!
 graph
